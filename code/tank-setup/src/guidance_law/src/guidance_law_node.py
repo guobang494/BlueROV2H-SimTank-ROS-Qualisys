fa76892646b83f10d6ed6e16f2c0e2ebe8f74ac9
grande-dev <grande.rdev@gmail.com>
2026-02-25 15:12:23 +0000

    [#4]
    Added implementation of the actual node.
    Modified CMkake and package files relevant to this pkg.

---
+++ code/tank-setup/src/guidance_law/src/guidance_law_node.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import rospy
+import yaml
+import math
+from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
+
+class GuidanceLawNode:
+
+    def __init__(self):
+        rospy.init_node("guidance_law_node")
+
+        # Load YAML file
+        config_file = rospy.get_param("~config_file")
+        with open(config_file, 'r') as f:
+            params = yaml.safe_load(f)
+
+        self.path_waypoint_following = params["path_waypoint_following"]
+        self.gamma = params["gamma"]
+        self.waypoints = params["waypoints"]
+        self.index = 0  # current waypoint index
+
+        # Current state
+        self.pos_x = 0.0
+        self.pos_y = 0.0
+        self.pos_z = 0.0
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
+
+        # Publishers: reference position & orientation
+        self.pub_ref_x = rospy.Publisher("/TODO/reference_position/linear/x", Float64, queue_size=10)
+        self.pub_ref_y = rospy.Publisher("/TODO/reference_position/linear/y", Float64, queue_size=10)
+        self.pub_ref_z = rospy.Publisher("/TODO/reference_position/linear/z", Float64, queue_size=10)
+        self.pub_ref_roll  = rospy.Publisher("/TODO/reference_position/angular/x", Float64, queue_size=10)
+        self.pub_ref_pitch = rospy.Publisher("/TODO/reference_position/angular/y", Float64, queue_size=10)
+        self.pub_ref_yaw   = rospy.Publisher("/TODO/reference_position/angular/z", Float64, queue_size=10)
+
+        # Publishers: diagnostic topics
+        self.pub_next_target = rospy.Publisher("/guidance_law_variables/next_target", Float64MultiArray, queue_size=10)
+        self.pub_distance = rospy.Publisher("/guidance_law_variables/distance_to_target", Float64, queue_size=10)
+
+        # Subscribers: state
+        rospy.Subscriber("/TODO/position/linear/x", Float64, self.cb_pos_x)
+        rospy.Subscriber("/TODO/position/linear/y", Float64, self.cb_pos_y)
+        rospy.Subscriber("/TODO/position/linear/z", Float64, self.cb_pos_z)
+        rospy.Subscriber("/TODO/position/angular/x", Float64, self.cb_roll)
+        rospy.Subscriber("/TODO/position/angular/y", Float64, self.cb_pitch)
+        rospy.Subscriber("/TODO/position/angular/z", Float64, self.cb_yaw)
+
+        rospy.loginfo("[guidance_law] Node started.")
+        self.loop()
+
+    # --- Callbacks ---
+    def cb_pos_x(self, msg): self.pos_x = msg.data
+    def cb_pos_y(self, msg): self.pos_y = msg.data
+    def cb_pos_z(self, msg): self.pos_z = msg.data
+    def cb_roll(self, msg):  self.roll = msg.data
+    def cb_pitch(self, msg): self.pitch = msg.data
+    def cb_yaw(self, msg):   self.yaw = msg.data
+
+    # --- Main control loop ---
+    def loop(self):
+        rate = rospy.Rate(20)  # 20 Hz
+        while not rospy.is_shutdown():
+
+            target = self.waypoints[self.index]
+            tx, ty, tz = target
+
+            # Compute distance to the next waypoint
+            dist = math.sqrt((tx - self.pos_x)**2 +
+                             (ty - self.pos_y)**2 +
+                             (tz - self.pos_z)**2)
+
+            # Publish diagnostic distance
+            self.pub_distance.publish(dist)
+
+            # Switch waypoint when the distance is < threshold
+            if dist < self.gamma and self.index < len(self.waypoints) - 1:
+                self.index += 1
+                rospy.loginfo("[guidance_law] Switching to waypoint %d" % self.index)
+                target = self.waypoints[self.index]
+                tx, ty, tz = target
+
+            # Compute reference orientation
+            ref_yaw = math.atan2(ty - self.pos_y, tx - self.pos_x)
+            ref_pitch = math.atan2(tz - self.pos_z,
+                                   math.sqrt((tx - self.pos_x)**2 +
+                                             (ty - self.pos_y)**2))
+            ref_roll = 0.0  # optional: can be extended later
+
+            # Publish reference position
+            self.pub_ref_x.publish(tx)
+            self.pub_ref_y.publish(ty)
+            self.pub_ref_z.publish(tz)
+
+            # Publish reference orientation
+            self.pub_ref_roll.publish(ref_roll)
+            self.pub_ref_pitch.publish(ref_pitch)
+            self.pub_ref_yaw.publish(ref_yaw)
+
+            # Publish next target waypoint
+            msg = Float64MultiArray()
+            msg.data = [tx, ty, tz, ref_roll, ref_pitch, ref_yaw]
+            self.pub_next_target.publish(msg)
+
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        GuidanceLawNode()
+    except rospy.ROSInterruptException:
+        pass
+
