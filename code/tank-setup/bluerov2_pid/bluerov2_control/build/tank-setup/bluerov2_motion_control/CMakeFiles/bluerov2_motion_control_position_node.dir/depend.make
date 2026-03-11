df9bf090dfc5bc4f88f5e594fd390f3c1dbf0e0c
grande-dev <grande.rdev@gmail.com>
2026-03-11 18:40:13 +0000

    [#11]
    Restructured repo with three main elements.

--- code/tank-setup/bluerov2_pid/bluerov2_control/build/tank-setup/bluerov2_motion_control/CMakeFiles/bluerov2_motion_control_position_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bluerov2_motion_control_position_node.
-# This may be replaced when dependencies are built.
--- code/tank-setup/bluerov2_pid/bluerov2_control/build/tank-setup/bluerov2_motion_control/CMakeFiles/bluerov2_motion_control_velocity_node.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for bluerov2_motion_control_velocity_node.
